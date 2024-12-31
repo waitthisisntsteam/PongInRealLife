1cbc306a43880a4590772b9bb1eb4f294b79f964
Erik.Gutierrez <Erik.Gutierrez@GMRDL4>
2024-12-30 18:17:12 -0800

    GAME FINISHED
    additional notes:
    
    physics a little wonky still trying to make faster swings better
    
    green still doesnt work

--- PongInRealLife/Form1.Designer.cs
+++ PongInRealLife/Form1.Designer.cs
@@ -30,6 +30,10 @@
         {
             components = new System.ComponentModel.Container();
             Display = new Emgu.CV.UI.ImageBox();
+            Player1ScoreLabel = new Label();
+            Player2ScoreLabel = new Label();
+            Player1Score = new Label();
+            Player2Score = new Label();
             ((System.ComponentModel.ISupportInitialize)Display).BeginInit();
             SuspendLayout();
             // 
@@ -38,24 +42,70 @@
             Display.BorderStyle = BorderStyle.FixedSingle;
             Display.Location = new Point(0, 0);
             Display.Name = "Display";
-            Display.Size = new Size(540, 360);
+            Display.Size = new Size(1080, 720);
             Display.SizeMode = PictureBoxSizeMode.StretchImage;
             Display.TabIndex = 3;
             Display.TabStop = false;
+            // 
+            // Player1ScoreLabel
+            // 
+            Player1ScoreLabel.AutoSize = true;
+            Player1ScoreLabel.Location = new Point(12, 9);
+            Player1ScoreLabel.Name = "Player1ScoreLabel";
+            Player1ScoreLabel.Size = new Size(83, 15);
+            Player1ScoreLabel.TabIndex = 4;
+            Player1ScoreLabel.Text = "Player 1 Score:";
+            // 
+            // Player2ScoreLabel
+            // 
+            Player2ScoreLabel.AutoSize = true;
+            Player2ScoreLabel.Location = new Point(985, 9);
+            Player2ScoreLabel.Name = "Player2ScoreLabel";
+            Player2ScoreLabel.Size = new Size(83, 15);
+            Player2ScoreLabel.TabIndex = 5;
+            Player2ScoreLabel.Text = "Player 2 Score:";
+            // 
+            // Player1Score
+            // 
+            Player1Score.AutoSize = true;
+            Player1Score.Location = new Point(12, 24);
+            Player1Score.Name = "Player1Score";
+            Player1Score.Size = new Size(13, 15);
+            Player1Score.TabIndex = 6;
+            Player1Score.Text = "0";
+            // 
+            // Player2Score
+            // 
+            Player2Score.AutoSize = true;
+            Player2Score.Location = new Point(1055, 24);
+            Player2Score.Name = "Player2Score";
+            Player2Score.RightToLeft = RightToLeft.Yes;
+            Player2Score.Size = new Size(13, 15);
+            Player2Score.TabIndex = 7;
+            Player2Score.Text = "0";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(541, 361);
+            ClientSize = new Size(1080, 721);
+            Controls.Add(Player2Score);
+            Controls.Add(Player1Score);
+            Controls.Add(Player2ScoreLabel);
+            Controls.Add(Player1ScoreLabel);
             Controls.Add(Display);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)Display).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
         private Emgu.CV.UI.ImageBox Display;
+        private Label Player1ScoreLabel;
+        private Label Player2ScoreLabel;
+        private Label Player1Score;
+        private Label Player2Score;
     }
 }
--- PongInRealLife/Form1.cs
+++ PongInRealLife/Form1.cs
@@ -1,6 +1,8 @@
 using Emgu.CV;
 using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 using Emgu.CV.Util;
+using System.Diagnostics.Eventing.Reader;
 using System.Text.RegularExpressions;
 
 namespace PongInRealLife
@@ -10,9 +12,14 @@ namespace PongInRealLife
         private VideoCapture Video;
 
         private Point BallCenter;
+        private Point BallSpeed;
         private int BallRadius;
         private int BallThickness;
-        private Emgu.CV.Structure.MCvScalar BallColor;
+        private MCvScalar BallColor;
+        private Rectangle BallHitBox;
+
+        private int Player1Wins;
+        private int Player2Wins;
 
         public Form1()
         {
@@ -23,10 +30,15 @@ namespace PongInRealLife
             Video.Set(CapProp.FrameHeight, 720);
             Video.Set(CapProp.Exposure, -6);
 
-            BallCenter = new Point(Display.Width / 2, Display.Height / 2);
+            BallSpeed = new Point(5, 5);
+            BallCenter = new Point(482, 275);
             BallRadius = 10;
             BallThickness = 20;
-            BallColor = new Emgu.CV.Structure.MCvScalar(0, 255, 0);
+            BallColor = new MCvScalar(0, 255, 0);
+            BallHitBox = new();
+
+            Player1Wins = 0;
+            Player2Wins = 0;
 
             Application.Idle += Update;
         }
@@ -40,32 +52,32 @@ namespace PongInRealLife
             {
                 case "red":
                     CvInvoke.InRange(input,
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(0,
-                                                                         0,
-                                                                         219)),
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(132,
-                                                                         157,
-                                                                         255)),
+                         new ScalarArray(new MCvScalar(0,
+                                             0,
+                                             169)),
+                         new ScalarArray(new MCvScalar(241,
+                                             64,
+                                             255)),
                          inputToInrange);
                     break;
                 case "blue":
                     CvInvoke.InRange(input,
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(116,
-                                                                         0,
-                                                                         0)),
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(255,
-                                                                         185,
-                                                                         78)),
+                         new ScalarArray(new MCvScalar(103,
+                                                       0,
+                                                       0)),
+                         new ScalarArray(new MCvScalar(255,
+                                                       255,
+                                                       66)),
                          inputToInrange);
                     break;
                 case "green":
                     CvInvoke.InRange(input,
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(0,
-                                                                         59,
-                                                                         0)),
-                         new ScalarArray(new Emgu.CV.Structure.MCvScalar(196,
-                                                                         171,
-                                                                         78)),
+                         new ScalarArray(new MCvScalar(0,
+                                                       41,
+                                                       0)),
+                         new ScalarArray(new MCvScalar(255,
+                                                       75,
+                                                       46)),
                          inputToInrange);
                     break;
                 default:
@@ -85,28 +97,89 @@ namespace PongInRealLife
             return new VectorOfPoint(Enumerable.Range(0, largestShape.Size).Select(i => largestShape[i]).ToArray());
         }
 
-        private IInputOutputArray DrawContours(List<string> coloredPaddles)
+        private IInputOutputArray DrawContours(List<string> coloredPaddles, out VectorOfVectorOfPoint hitBoxes)
         {
-            Mat output = Video.QueryFrame().Clone();
+            using Mat output = Video.QueryFrame().Clone();
 
-            List<List<Point>> contoursList = new();
             VectorOfVectorOfPoint contours = new();
             for (int i = 0; i < coloredPaddles.Count; i++)
             {
                 var currentPaddle = GetPaddleWithOutline(coloredPaddles[i]);
                 if (currentPaddle != null) contours.Push(currentPaddle);
             }
+            hitBoxes = contours;
 
-            CvInvoke.DrawContours(output, contours, -1, new Emgu.CV.Structure.MCvScalar(255, 255, 255), 10);
-            return output;
+            CvInvoke.DrawContours(output, contours, -1, new MCvScalar(255, 255, 255), 10);
+            return output.Clone();
+        }
+
+        private void BallPaddleCollision(Rectangle paddleHitBox)
+        {
+            if (BallHitBox.IntersectsWith(paddleHitBox))
+            {
+                if (BallHitBox.Right >= paddleHitBox.Left || BallHitBox.Left <= paddleHitBox.Right)
+                {
+                    if (BallHitBox.Bottom >= paddleHitBox.Top && BallHitBox.Top <= paddleHitBox.Bottom)
+                    {     
+                        BallSpeed.X *= -1;
+                        if (!(BallHitBox.Bottom >= 550 || BallHitBox.Top <= Display.Top)) BallCenter.X += BallSpeed.X * 3;
+                        BallSpeed.X *= 2;
+                    }
+                }
+                if (BallHitBox.Top <= paddleHitBox.Bottom)
+                {
+                    if (BallHitBox.Left >= paddleHitBox.Left && BallHitBox.Right <= paddleHitBox.Right)
+                    {
+                        BallSpeed.Y *= -1;
+                        if (!(BallHitBox.Bottom >= 550 || BallHitBox.Top <= Display.Top)) BallCenter.Y += BallSpeed.Y * 3;
+                        BallSpeed.Y *= 2;
+                    }
+                }
+            }
         }
 
         private void Update(object sender, EventArgs e)
         {
-            using var currentScreen = DrawContours(["blue", "red", "green"]);
-            CvInvoke.Circle(currentScreen, BallCenter, BallRadius, BallColor, BallThickness);
+            VectorOfVectorOfPoint hitBoxes = new();
+            using var currentScreen = DrawContours(["blue", "red"/*, "green"*/], out hitBoxes);
 
+            CvInvoke.Circle(currentScreen, BallCenter, BallRadius, BallColor, BallThickness);
+            for (int i = 0; i < hitBoxes.Size; i++)
+            {
+                Rectangle currentPaddle = CvInvoke.BoundingRectangle(hitBoxes[i]);
+                CvInvoke.Rectangle(currentScreen, currentPaddle, new MCvScalar(255, 0, 255), 10);
+
+                BallHitBox = new Rectangle(BallCenter.X - BallRadius, BallCenter.Y - BallRadius, BallRadius * 2, BallRadius * 2);
+                CvInvoke.Rectangle(currentScreen, BallHitBox, new MCvScalar(255, 0, 255), 10);
+                BallPaddleCollision(currentPaddle);
+            }
+
+            if (BallHitBox.Bottom >= 550 || BallHitBox.Top <= Display.Top) BallSpeed.Y *= -1;
+
+            //win conditions
+            if (BallHitBox.Left <= Display.Left)
+            {
+                Player1Wins++;
+                Player1Score.Text = Player1Wins.ToString();
+                BallCenter = new Point(482, 275);
+                BallSpeed = new Point(-5, -5);
+            }
+            if (BallHitBox.Right >= 965)
+            {
+                Player2Wins++;
+                Player2Score.Text = Player2Wins.ToString();
+                BallCenter = new Point(482, 275);
+                BallSpeed = new Point(5, 5);
+            }
+
+            //deceleration
+            if (BallSpeed.X > 5) BallSpeed.X--;
+            if (BallSpeed.Y > 5) BallSpeed.Y--;
+            BallCenter.Offset(BallSpeed);
+
+            CvInvoke.Flip(currentScreen, currentScreen, FlipType.Horizontal);
             Display.DisplayedImage = currentScreen;
+            hitBoxes.Dispose();
         }
     }
 }
